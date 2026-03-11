f4c11b80fffa016a9fe05f768ce87153389e09e6
gabrielle-lima <gaibou_lima@hotmail.com>
2026-03-11 02:34:07 -0300

    feat: create new migrations for schemas

--- betalenttest/database/migrations/1773201938686_create_transactions_table.ts
+++ betalenttest/database/migrations/1773201938686_create_transactions_table.ts
@@ -6,6 +6,7 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
+      table.number('gateway_id')
       table.number('external_id').unsigned()
       table.string('status').notNullable()
       table.number('amount').notNullable()
